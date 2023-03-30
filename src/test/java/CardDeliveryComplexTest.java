ff980f3c56396acbd5ab04e651956c3c52a80255
Khalilova <sharova-ira90@yandex.ru>
2023-03-30 21:33:00 +0300

    edit code

--- src/test/java/CardDeliveryComplexTest.java
+++ src/test/java/CardDeliveryComplexTest.java
@@ -1,28 +1,30 @@
 
 import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Keys;
 
 
 import java.time.Duration;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryComplexTest {
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
     NeedDay day = new NeedDay();
     String planningDate = day.generateDate(7, "dd.MM.yyyy");
 
-    LocalDate currentDate = LocalDate.now();
+    LocalDate currentDate = LocalDate.now().plusDays(3);
     LocalDate planning = LocalDate.now().plusDays(7);
 
 
 
     @Test
     public void shoudRegistred1() {
-        open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Йо");
         $x("//*[contains(text(),'Йошкар')]").click();
         $("[data-test-id='date'] input").click();
@@ -36,7 +38,6 @@ public class CardDeliveryComplexTest {
         $("[data-test-id=phone] input").setValue("+79189871057");
         $x("//*[contains(text(),'Я соглашаюсь с условиями')]").click();
         $x("//*[contains(text(),'Забронировать')]").click();
-        $("[data-test-id=notification]").should(Condition.appear, Duration.ofSeconds(15));
         $(".notification__content")
                 .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
                 .shouldBe(Condition.visible);
--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -9,12 +10,15 @@ import java.time.Duration;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
     NeedDay day = new NeedDay();
     String planningDate = day.generateDate(3, "dd.MM.yyyy");
 
     @Test
     public void shoudRegistred() {
-        open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Йошкар-Ола");
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.DELETE);
         $("[data-test-id=date] input").setValue(planningDate);
@@ -22,7 +26,6 @@ public class CardDeliveryTest {
         $("[data-test-id=phone] input").setValue("+79189871057");
         $x("//*[contains(text(),'Я соглашаюсь с условиями')]").click();
         $x("//*[contains(text(),'Забронировать')]").click();
-        $("[data-test-id=notification]").should(Condition.appear, Duration.ofSeconds(15));
         $(".notification__content")
                 .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
                 .shouldBe(Condition.visible);
